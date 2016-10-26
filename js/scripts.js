85507893beb2d5e997bac663c6bb65967d3ebe69
Kevin Finley and Maxwell Cady <kevin+maxwell@>
2016-10-26 16:12:26 -0700

    finish basic logic for pig latin words

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,65 @@ var pigLatin = function(input) {
   var splitInput = lowercaseInput.split("");
   var vowels = ["a", "e", "i", "o", "u", "y"];
   var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+  var wordsTogether = [];
 
-  var firstChecker = function(splitInput) {
-    if (splitInput[0] === "q" && splitInput[1] === "u") {
-      splitInput.shift(splitInput.push(splitInput[0]));
-      splitInput.shift(splitInput.push(splitInput[0]));
-    } else if (consonants.indexOf(splitInput[0]) > -1) {
-      splitInput.shift(splitInput.push(splitInput[0]));
-      firstChecker(splitInput);
+
+  var words = input.split(" ");
+
+  words.forEach(function(word) {
+    var splitWord = word.split("");
+
+    var firstChecker = function(splitWord) {
+      if (splitWord[0] === "q" && splitWord[1] === "u") {
+        splitWord.shift(splitWord.push(splitWord[0]));
+        splitWord.shift(splitWord.push(splitWord[0]));
+      } else if (consonants.indexOf(splitWord[0]) > -1) {
+        splitWord.shift(splitWord.push(splitWord[0]));
+        firstChecker(splitWord);
+      }
     }
-  }
-    if (splitInput[0] === "y") {
-      splitInput.shift(splitInput.push(splitInput[0]));
-      splitInput.push("ay");
-    } else if (vowels.indexOf(splitInput[0]) > -1) {
-    splitInput.push("ay");
-    } else {
-    firstChecker(splitInput);
-    splitInput.push("ay")
-  }
-    console.log(splitInput);
+      if (splitWord[0] === "y") {
+        splitWord.shift(splitWord.push(splitWord[0]));
+        splitWord.push("ay");
+      } else if (vowels.indexOf(splitWord[0]) > -1) {
+      splitWord.push("ay");
+      } else {
+      firstChecker(splitWord);
+      splitWord.push("ay")
+    }
+      splitWord[0] = splitWord[0].toUpperCase();
+      var output = splitWord.join("");
+      wordsTogether.push(output);
+    })
+    var finishedWords = wordsTogether.join(" ");
+    console.log(finishedWords);
 }
+
+
+
+
+  // var firstChecker = function(splitWord) {
+  //   if (splitWord[0] === "q" && splitWord[1] === "u") {
+  //     splitWord.shift(splitWord.push(splitWord[0]));
+  //     splitWord.shift(splitWord.push(splitWord[0]));
+  //   } else if (consonants.indexOf(splitWord[0]) > -1) {
+  //     splitWord.shift(splitWord.push(splitWord[0]));
+  //     firstChecker(splitWord);
+  //   }
+  // }
+  //   if (splitWord[0] === "y") {
+  //     splitWord.shift(splitWord.push(splitWord[0]));
+  //     splitWord.push("ay");
+  //   } else if (vowels.indexOf(splitWord[0]) > -1) {
+  //   splitWord.push("ay");
+  //   } else {
+  //   firstChecker(splitWord);
+  //   splitWord.push("ay")
+  // }
+  //   splitWord[0] = splitWord[0].toUpperCase();
+  //   var output = splitWord.join("");
+  //   console.log(output);
+
 
 
 // Front end
