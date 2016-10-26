7a5ff9d6e4643f1007da27e875af65cfef52527b
Kevin Finley and Maxwell Cady <maxwell@>
2016-10-26 15:24:25 -0700

    create function to check first letters until array reaches a vowel, then moves them to the end of the array and adds ay

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-1) Do nothing to non letter characters in string
+!!1) Do nothing to non letter characters in string
 input: 3
 output: 3
 
-1) Check if the first letter is a vowel. If it is, add "ay" to the end
+!!2) Check if the first letter is a vowel. If it is, add "ay" to the end
 input: elephant
 out: elephantay
 
-3) If word begins in one or more consonants, move them all to the end, and add "ay"
+2.5) Move all letters before first vowel to the end of the word
+
+!!3) If word begins in one or more consonants, move them all to the end, and add "ay"
 input: frog
 output: ogfray
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
-var pigLatin = function(string) {
-  var letters = string.toLowerCase().split("");
+var pigLatin = function(input) {
+
+  var lowercaseInput = input.replace(/[^a-z\s]/g, "").toLowerCase();
+  var splitInput = lowercaseInput.split("");
   var vowels = ["a", "e", "i", "o", "u"];
   var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-  var vowelsLocation = [];
 
-  if (vowels.indexOf(letters[0]) > -1 ) {
-    letters.push("ay");
+  var firstChecker = function(splitInput) {
+    if (consonants.indexOf(splitInput[0]) > -1) {
+      splitInput.shift(splitInput.push(splitInput[0]));
+      firstChecker(splitInput);
+    }
   }
 
-  consonants.forEach(function(consonant) {
-    if (consonant = letters[0]) {
-      letters.shift(letters.push(letters[0]));
-    }
-  })
-  // for (i = 0; i < letters.length; i++) {
-  //   if (vowels.indexOf(letters(i)) > -1) {
-  //     vowelsLocation.push(vowels[i]);
-  //   }
-  // }
-    console.log(vowelsLocation);
-    console.log(letters);
+  if (vowels.indexOf(splitInput[0]) > -1) {
+    splitInput.push("ay");
+  } else {
+    firstChecker(splitInput);
+    splitInput.push("ay")
+  }
+    console.log(splitInput);
 }
 
 
-
+// Front end
 $(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
-    originalString = $("#input input").val();
+    var originalString = $("#input input").val();
     pigLatin(originalString);
   })
 });
